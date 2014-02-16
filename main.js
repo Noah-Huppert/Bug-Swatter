31474e193ec90d1d07ddcc97757c0f14cfec852e
Noah-Huppert <noahhuppert@gmail.com>
2014-02-15 23:12:17 -0500

    Started on V2

--- main.js
+++ main.js
@@ -1,139 +1,145 @@
-/* The local API for BS */
+function bs(DEBUG_SET){
+	//Set DEBUG status
+	this.DEBUG;
+	if(DEBUG_SET != undefined){
+		this.DEBUG = DEBUG_SET;
+	} else{
+		this.DEBUG = false;
+	}
+	
+	var db = new jsonDB("tasks", task);
+	var clear = new status("Clear", false);
+	var tempTask = new task(324, clear);
+	/*db.add(tempTask);
+	db.add(new task(322, clear));
+	db.add(new task(325, new status("Clear", false)));
+	console.log(db.data);*/
+	
+}
 
-function task(id, status){
-	this.id = id;
-	this.status = status;
-	this.lastMod = newDate().getTime();
+function jsonDB(sName, sObject){
+	this.name = sName;//The name of the database
+	this.object = sObject;//The object type you will be storing
+	this.namespace = chrome.storage.sync;//Quicker way to use browser storage
+	this.data = [];
+	
+	this.init = function(){
+		var name = this.name;
+		var data = [];
+		this.namespace.get(this.name, function(result){
+			if(result != undefined){//Check if jsonDB exists
+				data = result[name].data;
+				console.log(data);
+			}
+		});	
+		console.log(data);
+		this.data = data;
+	};
+	
+	
+	//Getters
+	this.getName = function(){
+		return this.name;
+	};
+	
+	this.getObject = function(){
+		return this.object;
+	};
+	
+	this.get = function(sIndex, sValue){
+		var tempResults = [];
+		for(var key in this.data){
+			if(this.data[key][sIndex] == sValue){
+				tempResults.push(this.data[key]);
+			}
+		}
+		
+		if(tempResults.length == 1){
+			return tempResults[0];
+		} else{
+			return tempResults;
+		}
+	};
+	
+	
+	//Setters
+	this.add = function(sObject){
+		if($.inArray(sObject, this.data) == -1){//Duplicate check
+			this.data.push(sObject);
+		}
+		this.save();
+	};
+	
+	this.remove = function(sObject){
+		if($.inArray(sObject) != -1){//Check to make sure object exists in db
+			this.data.splice(this.data.indexOf(sObject), 1);
+		}
+		this.save();
+	};
+	
+	this.save = function(){
+		var sname = this.name;
+		var sdata = this.data;
+		this.namespace.set({ sname: sdata });
+	};
+	
+	this.init();
+	console.log(this.data);
+}
+
+function task(sID, sStatus){
+	this.id = sID;
+	this.status = sStatus;
+	this.lastMod = new Date().getTime();
+	
+	
+	//Getters
+	this.getID = function(){
+		return this.id;
+	};
+	
+	this.getStatus = function(){
+		return this.status;
+	};
+	
+	
+	//Setters
+	this.setStatus = function(sStatus){
+		this.status = sStatus;
+		this.lastMod = new Date().getTime();
+	};
+	
+	
+	//Actions
+	//TODO add actions
 }
 
 function status(sName, sShared){
 	this.name = sName;
 	this.shared = sShared;
 	
-	this.setShared = function(sShare){
-		status.shared = sShare;
+	
+	//Getters
+	this.getName = function(){
+		return this.name;
 	};
+	
+	this.getShared = function(){
+		return this.shared;
+	};
+	
+	
+	//Setters
+	this.setName = function(sName){
+		this.name = sName;
+	};
+	
+	this.setShared = function(sShared){
+		this.shared = sShared;
+	};
+	
+	
+	//Actions
+	//TODO add actions
 }
 
-function bs(DEBUG_SET){
-	this.db;//Used for IndexedDB functions
-	this.DEBUG;//To set output
-	
-	//Set database varribles for later
-	this.tasks;
-	this.statuses;
-	
-	if(DEBUG_SET != undefined){
-		this.DEBUG = DEBUG_SET;	
-	} else{
-		this.DEBUG = false;
-	}
-	
-	/* DB Actions */
-	this.addToDB = function(db, object){
-		var request = bs.db.add(object);
-		
-		request.onerror = function(event){
-			bs.log(event.target, "object");
-		};
-		
-		request.onsuccess = function(event){
-			bs.log("Added " + object + " to " + db);
-		};
-	};
-	
-	this.removeFromDB = function(db, object){
-		bs.log("Function bs.removeFromDB(db, object) not implimented yet\nRequested DB: " + db + "\nRequested OBJECT: " + object, true);
-	};
-	/***********/
-	
-	
-	/* Status Actions */
-	this.addStatus = function(status){
-		bs.addToDB(bs.statuses, status);
-	};
-	
-	this.removeStatus = function(status){
-		bs.removeFromDB(bs.statuses, status);
-	};
-	/**********/
-	
-	
-	/* Task Actions */
-	this.addTask= function(task){
-		bs.addToDB(bs.tasks, task);
-	};
-	
-	this.removeTask = function(task){
-		bs.log("Function bs.removeTask(task) not implimented yet.", true);
-	};
-	/**********/
-	
-	
-	/* BS Actions */
-	this.alert = function(alertString){
-		alert(alertString);
-	};
-	
-	this.log = function(logString, userError){//userError <-- Display in console, disregaurding DEBUG
-		if(bs.DEBUG == true){
-			if(userError != undefined){
-				switch(userError){
-					case true:
-						console.log("Bug Swatter ERROR: " + logString);
-					break;
-					
-					case "object":
-						console.log("Bug Swatter ERROR: ");
-						console.log(userError);
-					break;
-				}
-			} else{
-				console.log("BS: " + logString);
-			}
-		} else{
-			if(userError != undefined && userError == true){
-				console.log("Bug Swatter ERROR: " + logString);
-			}
-		}
-	};
-	/**********/
-	
-	
-	this.init = function(){//Initalize IndexedDB
-		if("indexedDB" in window){
-			var request = indexedDB.open("BS_DB", 1);//Get handle
-			
-			request.onupgradeneeded = function(event){//First time setup
-				bs.log("Setting up database");
-				var thisDB = event.target.result;
-				
-				if(!thisDB.objectStoreNames.contains("tasks")){
-					thisDB.createObjectStore("tasks", { keyPath: "id"});
-				}
-				
-				if(!thisDB.objectStoreNames.contains("statuses")){
-					thisDB.createObjectStore("statuses", { keyPath: "name"});
-				}
-			};
-			
-			request.onsuccess = function(event){
-				bs.log("Connected to db");
-				bs.db = event.target.result;
-				
-				//bs.statuses = 
-			};
-			
-			request.onerror = function(event){
-				bs.log(event.target, "object");
-			};
-		} else{
-			bs.log("A crucial library(IndexDB) is not supported in your browser", true);
-		}
-	};
-	
-	
-	/* Constructor */
-	this.init();
-	/**************/
-}
