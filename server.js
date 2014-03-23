ba66f88da9e66cd0044057230c74befd9af084e7
Noah-Huppert <noahhuppert@gmail.com>
2014-03-23 16:32:34 -0400

    Fixed port number

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/public'));
 
-server.listen(8080, function(){
+var port = Number(process.env.PORT || 5000);
+
+server.listen(port, function(){
 	console.log('Listening on port %d', server.address().port);
 });
 
